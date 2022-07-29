b78bbae84ea2ec78b3a696dd726df34833ee9608
genaroferreira <genaroferreira36@gmail.com>
2022-07-29 17:28:58 -0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,24 @@
+let diadelasemana = prompt("Que dia es?");
+
+switch (diadelasemana){
+    case "lunes": alert("hoy es lunes: rutina de pecho");
+    break;
+    case "martes": alert("hoy es martes: rutina de piernas");
+    break;
+    case "miercoles": alert("hoy es miercoles: rutina de espalda");
+    break;
+    case "jueves": alert("hoy es jueves: rutina de brazos");
+    break;
+    case "viernes": alert("hoy es viernes: rutina de abdomen");
+    break;
+    case "sabado": alert("hoy es sabado: descanzo");
+    break;
+    case "domingo": alert("hoy es domingo: descanzo");
+    break;
+    default: alert("escribe un dia de la semana en minusculas");
+}
+
+
+for(i=20;i<=70;i++){
+    document.write("hola mundo"+i+"<br>");
+}
